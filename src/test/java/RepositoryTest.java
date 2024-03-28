b8f994fff4a15f2bf37ddc959f1acceedb3dfa5d
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-28 12:57:27 +0100

    updating repository

--- src/test/java/RepositoryTest.java
+++ src/test/java/RepositoryTest.java
@@ -1,16 +1,19 @@
 
+import data.model.User;
 import data.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+import static org.junit.Assert.assertEquals;
 
 @DataMongoTest
 public class RepositoryTest{
-    @Autowired
     private UserRepository userRepository;
     @Test
     public void saveUser_testUserIsSaved(){
-
+        User nweUser = new User();
+        userRepository.save(nweUser);
+        assertEquals(1, userRepository.count());
     }
 
 }
