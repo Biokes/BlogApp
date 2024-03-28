2b9c5fe7a262191af0ccff9bb684a105daacdd32
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-28 18:51:17 +0100

    updating repositories

--- src/main/java/blogPack/data/model/Comments.java
+++ src/main/java/blogPack/data/model/Comments.java
@@ -1,8 +1,18 @@
 package blogPack.data.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+
 @Data
+@Document("Comments")
 public class Comments{
+    @Id
     private String id;
+    @DBRef
     private User commenter;
+    private String commentBody;
+    private LocalDateTime timeOfComment;
 }
