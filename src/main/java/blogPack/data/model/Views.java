e38a78dcf5ae747240750972328b0425e9493d54
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-28 18:52:43 +0100

    updating repositories

--- src/main/java/blogPack/data/model/Views.java
+++ src/main/java/blogPack/data/model/Views.java
@@ -1,10 +1,17 @@
 package blogPack.data.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 @Data
+@Document("Views")
 public class Views{
+    @Id
+    private String id;
     private LocalDateTime timeViewed;
+    @DBRef
     private User viewer;
 }
