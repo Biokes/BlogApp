f712628ff8bce9f64ece30c1d3a5a0294f1487db
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-28 19:24:07 +0100

    updating repositories

---
+++ src/test/java/blogPack/data/repositories/UserRepositoryTest.java
@@ -0,0 +1,12 @@
+package blogPack.data.repositories;
+
+
+import org.junit.jupiter.api.Test;
+
+class UserRepositoryTest{
+    private UserRepository userRepository;
+    @Test
+    public void saveUser_testUserIsSaved(){
+
+    }
+}
