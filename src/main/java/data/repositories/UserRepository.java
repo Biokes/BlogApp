a3dd9786c3108c566d492d4cde550fe017fb4abd
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-28 12:54:41 +0100

    updating repository

--- src/main/java/data/repositories/UserRepository.java
+++ src/main/java/data/repositories/UserRepository.java
@@ -3,6 +3,6 @@ package data.repositories;
 import data.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
+@Repository
 public interface UserRepository extends MongoRepository<User, String>{
 }
