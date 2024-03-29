0a810d068aa8dcc0120370685a39593edb9b65fe
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-29 10:33:57 +0100

    updating blog services

--- src/main/java/blogPack/services/PostServicesImplements.java
+++ src/main/java/blogPack/services/PostServicesImplements.java
@@ -5,4 +5,8 @@ import org.springframework.stereotype.Service;
 @Service
 
 public class PostServicesImplements implements PostServices{
+    @Override
+    public long count(){
+        return 0;
+    }
 }
--- src/test/java/blogPack/data/repositories/services/UserServicesTest.java
+++ src/test/java/blogPack/data/repositories/services/UserServicesTest.java
@@ -1,7 +1,9 @@
 package blogPack.data.repositories.services;
 
 import blogPack.data.model.Post;
+import blogPack.dto.PostRequest;
 import blogPack.dto.RegisterRequest;
+import blogPack.services.PostServices;
 import blogPack.services.UserServices;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class UserServicesTest{
     @Autowired
     private UserServices userServices;
+    @Autowired
+    private PostServices postServices;
     @Test
     void createUser_testUserIsCreated(){
         RegisterRequest request = new RegisterRequest();
@@ -31,9 +35,11 @@ public class UserServicesTest{
         request.setLastName("LastName");
         request.setPassword("my password");
         userServices.createUser(request);
+        Post post = new Post();
         assertEquals(1,userServices.count());
-        PostRequest post = new PostRequest();
-        user.createPost()
-
+        PostRequest postRequest = new PostRequest();
+        post.setTitle(postRequest.getTitle( ));
+        postRequest.setContent(postRequest.getContent( ));
+        assertEquals(1, postServices.count());
     }
 }
