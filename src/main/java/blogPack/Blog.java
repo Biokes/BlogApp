b024ce3d5964b0b00f63e075fa4d7bc0cfa7ed1b
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-29 09:07:13 +0100

    updating repositories

---
+++ src/main/java/blogPack/Blog.java
@@ -0,0 +1,10 @@
+package blogPack;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog{
+    public static void main(String[] args){
+
+    }
+}
--- src/test/java/blogPack/data/repositories/repositories/UserRepositoryTest.java
+++ src/test/java/blogPack/data/repositories/repositories/UserRepositoryTest.java
@@ -1,17 +1,27 @@
 package blogPack.data.repositories.repositories;
 
 
+import blogPack.data.model.User;
 import blogPack.data.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@DataMongoTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest
 class UserRepositoryTest{
     @Autowired
-    private UserRepository userRepository;
+    private  UserRepository userRepository ;
     @Test
     public void saveUser_testUserIsSaved(){
-
+        User user = new User();
+        User user2 = new User();
+        userRepository.save(user);
+        userRepository.save(user2);
+        assertEquals(2,userRepository.count());
     }
 }
