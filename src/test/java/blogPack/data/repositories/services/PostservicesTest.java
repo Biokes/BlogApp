a52c2d4efffd3b29366e88fe71b22685e4cd1916
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-30 07:52:30 +0100

    updating blog post services

--- src/test/java/blogPack/data/repositories/services/PostservicesTest.java
+++ src/test/java/blogPack/data/repositories/services/PostservicesTest.java
@@ -2,6 +2,7 @@ package blogPack.data.repositories.services;
 
 import blogPack.data.model.Comment;
 import blogPack.data.model.Post;
+import blogPack.data.model.User;
 import blogPack.dto.CommentRequest;
 import blogPack.dto.DeletePostRequest;
 import blogPack.dto.PostRequest;
@@ -62,21 +63,5 @@ public class PostservicesTest{
         postServices.deletePost(deletePostRequest);
         assertEquals(0,postServices.count());
     }
-    @Test
-    void commentOnPost_testPostIsCommenetedOn(){
-        Post post = new Post();
-        PostRequest postRequest = new PostRequest();
-        post.setPoster("userName");
-        postRequest.setTitle("post Title.");
-        post.setTitle(postRequest.getTitle( ));
-        postRequest.setContent(postRequest.getContent( ));
-        postServices.save(post);
-        assertEquals(1, postServices.count());
-        Comment comment = new Comment();
-        CommentRequest commentRequest = new CommentRequest();
-        commentRequest.setCommentBody("nice");
-        comment.setCommenter("userName");
-        Mappers.mapComment(comment, commentRequest);
 
-    }
 }
--- src/test/java/blogPack/data/repositories/services/UserServicesTest.java
+++ src/test/java/blogPack/data/repositories/services/UserServicesTest.java
@@ -1,6 +1,9 @@
 package blogPack.data.repositories.services;
 
+import blogPack.data.model.Comment;
 import blogPack.data.model.Post;
+import blogPack.data.model.User;
+import blogPack.dto.CommentRequest;
 import blogPack.dto.DeletePostRequest;
 import blogPack.dto.PostRequest;
 import blogPack.dto.RegisterRequest;
@@ -9,6 +12,7 @@ import blogPack.services.UserServices;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import utilities.Mappers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,6 +20,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class UserServicesTest{
     @Autowired
     private UserServices userServices;
+    @Autowired
+    private PostServices postServices;
     @Test
     void createUser_testUserIsCreated(){
         RegisterRequest request = new RegisterRequest();
@@ -26,5 +32,21 @@ public class UserServicesTest{
         userServices.createUser(request);
         assertEquals(1,userServices.count());
     }
-
+    @Test
+    void commentOnPost_testPostIsCommenetedOn(){
+        Post post = new Post();
+        PostRequest postRequest = new PostRequest();
+        post.setPoster("userName");
+        postRequest.setTitle("post Title.");
+        post.setTitle(postRequest.getTitle( ));
+        postRequest.setContent(postRequest.getContent( ));
+        postServices.save(post);
+        assertEquals(1, postServices.count());
+        Comment comment = new Comment();
+        CommentRequest commentRequest = new CommentRequest();
+        commentRequest.setCommentBody("nice");
+        comment.setCommenter("userName");
+        Mappers.mapComment(comment, commentRequest);
+        User user = new User();
+    }
 }
