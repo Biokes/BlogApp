576ac9546b088e05b7cb2c117c9b00e3d8b3dc25
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-30 07:12:36 +0100

    updating blog services

--- src/main/java/blogPack/services/PostServicesImplements.java
+++ src/main/java/blogPack/services/PostServicesImplements.java
@@ -21,7 +21,6 @@ public class PostServicesImplements implements PostServices{
     public void save(Post post){
         postRepositpory.save(post);
     }
-
     @Override
     public void deleteAll(){
         postRepositpory.deleteAll();
