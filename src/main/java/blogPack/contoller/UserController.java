bd8ab87cac430d898ee2774b9a18348e9006ea58
Biokes <106444765+Biokes@users.noreply.github.com>
2024-04-03 07:50:28 +0100

    update Controller end points

--- src/main/java/blogPack/contoller/UserController.java
+++ src/main/java/blogPack/contoller/UserController.java
@@ -1,20 +1,15 @@
 package blogPack.contoller;
-
-
-import blogPack.dto.RegisterRequest;
+import blogPack.dto.*;
 import blogPack.exception.BlogExceptions;
-import blogPack.services.BlogUserService;
 import blogPack.services.UserServices;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 @RestController
+@RequestMapping("/api/v1")
 @AllArgsConstructor
 public class UserController{
     private UserServices userServices;
-    @PostMapping("/Blog-Register_user")
+    @PostMapping("/Blog-Register-user")
     public String registerUser(@RequestBody RegisterRequest registerRequest){
         try{
            userServices.createUser(registerRequest);
@@ -23,7 +18,63 @@ public class UserController{
         }
         return "successfully created";
     }
+    @DeleteMapping("/delete-Post")
+    public String deletePost(@RequestBody DeletePostRequest deletePostRequest){
+        try{
+            userServices.deletePostWith(deletePostRequest);
+            return "deleted successfully";
+        }catch(BlogExceptions error){
+            return error.getMessage();
+        }
+    }
+    @GetMapping("/view-post")
+    public String viewPost(@RequestBody ViewRequest viewRequest){
+        try{
+            ViewPostResponse viewPostResponse = userServices.viewPost(viewRequest);
+            return viewPostResponse.toString();
+        }catch(BlogExceptions error){
+            return error.getMessage();
+        }
+    }
+    @PatchMapping("/comment-on-post")
+    public String commentOnPost(@RequestBody CommentRequest commentRequest){
+        try{
+            userServices.commentOnPostWith(commentRequest);
+            return "commment added to post";
+        }catch(BlogExceptions error){
+            return error.getMessage();
+        }
+    }
+    @RequestMapping("/update-post")
+    public String updatePostRequest(@RequestBody UpdatePostRequest updatePostRequest){
+        try{
+            userServices.updatePost(updatePostRequest);
+            return "updated successfully.";
+        }
+        catch(BlogExceptions error){
+            return error.getMessage();
+        }
+    }
+    @RequestMapping("/create-post")
+    public String createPost(@RequestBody PostRequest postRequest){
+        try{
+            userServices.savePost(postRequest);
+            return "post saved";
+        }
+        catch(BlogExceptions error){
+            return error.getMessage();
+        }
+    }
+    @DeleteMapping("/delete-comment")
+    public String deleteComment(@RequestBody DeleteCommentRequest deleteComent){
+        try{
+            userServices.deleteComment(deleteComent);
+            return "deleted successfully";
+        }
+        catch(BlogExceptions error){
+            return error.getMessage();
+        }
 
-
+    }
 }
 
