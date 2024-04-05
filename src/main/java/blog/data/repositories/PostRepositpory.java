4258acf1058293db6dd2c9b11a89f16a96ff6588
Biokes <106444765+Biokes@users.noreply.github.com>
2024-04-05 18:03:29 +0100

    update delete post and using optional class

--- src/main/java/blog/data/repositories/PostRepositpory.java
+++ src/main/java/blog/data/repositories/PostRepositpory.java
@@ -5,11 +5,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PostRepositpory extends MongoRepository<Post, String>{
     List<Post> findPostsByPoster(String posterName);
 
-    Post findPostByTitle(String postTitle);
+    Optional<Post> findPostByTitle(String postTitle);
 
 }
